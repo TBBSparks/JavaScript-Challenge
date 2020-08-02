3f20ef25bbe2b85d83b13cee6cf41809e3d5f22a
Tina Sparks <61213724+TBBSparks@users.noreply.github.com>
2020-08-02 13:38:11 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -54,6 +54,26 @@ dropdown_click3.on("click", function()
     d3.select(".check-input").text("");
     tableItem = 2;
 });
+dropdown_click4.on("click", function()
+{
+    d3.select("input").attr("placeholder", "Enter a country");
+    d3.select(".form-control").property("value", "");
+    dropdown_click.style("background-color", "transparent");
+    dropdown_click2.style("background-color", "#DF691A");
+    d3.select("#dropdownMenuButton").style("background-color", "cornflowerblue");
+    d3.select(".check-input").text("");
+    tableItem = 3;
+});
+dropdown_click5.on("click", function()
+{
+    d3.select("input").attr("placeholder", "Enter a shape");
+    d3.select(".form-control").property("value", "");
+    dropdown_click.style("background-color", "transparent");
+    dropdown_click2.style("background-color", "#DF691A");
+    d3.select("#dropdownMenuButton").style("background-color", "cornflowerblue");
+    d3.select(".check-input").text("");
+    tableItem = 4;
+});
 
 let old_tbody = document.getElementsByTagName("table")[0];
 
